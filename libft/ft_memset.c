5053570d9360413caacd7096ef5693c0eae6277b
okim <okim@student.42seoul.kr>
2020-11-16 21:52:20 +0900

    modified 201116 21:52

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 09:57:06 by okim              #+#    #+#             */
-/*   Updated: 2020/11/14 13:27:28 by okim             ###   ########.fr       */
+/*   Updated: 2020/11/16 20:55:51 by okim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,7 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t i;
-
-	i = 0;
-	while (i < len)
-	{
-		*((char *)b + (sizeof(char) * i)) = c;
-		i++;
-	}
+	while (len--)
+		*((char *)b + (sizeof(char) * len)) = c;
 	return (b);
 }

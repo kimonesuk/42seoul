4f986bce4761979a0c4b3d505211b3c9e97c99e4
okim <okim@student.42seoul.kr>
2020-11-18 22:48:41 +0900

    ft_strnstr add 201118 22:48

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 22:41:39 by okim              #+#    #+#             */
-/*   Updated: 2020/11/17 23:12:33 by okim             ###   ########.fr       */
+/*   Updated: 2020/11/18 22:47:05 by okim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	h_len;
 	size_t	n_len;
+	size_t	h_len;
 	size_t	i;
 
 	if (*needle == 0)
 		return ((char *)haystack);
-	h_len = ft_strlen(haystack);
 	n_len = ft_strlen(needle);
-	if (h_len < len || len < n_len)
+	h_len = ft_strlen(haystack);
+	if (len < n_len || h_len < n_len)
 		return (0);
 	i = 0;
 	while (haystack[i] != 0 && i < len)

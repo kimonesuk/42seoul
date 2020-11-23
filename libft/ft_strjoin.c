2802b6286148fde960b5bd88fe59da8f06c9e68c
okim <okim@student.42seoul.kr>
2020-11-23 16:37:25 +0900

    ft_strjoin finished 201123 16:37

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 17:29:37 by okim              #+#    #+#             */
-/*   Updated: 2020/11/22 17:30:15 by okim             ###   ########.fr       */
+/*   Updated: 2020/11/23 16:30:49 by okim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,17 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	return ();
+	char *jnstr;
+	int	len1;
+	int	len2;
+
+	if (!s1 || !s2)
+		return (0);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (!(jnstr = (char *)malloc(sizeof(char) * (len1 + len2 + 1))))
+		return (0);
+	ft_strlcpy(jnstr, s1, len1 + 1);
+	ft_strlcat(jnstr, s2, len1 + len2 + 1);
+	return (jnstr);
 }

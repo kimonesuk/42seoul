f8adf471708707854dc931d3244deccd41d83d27
okim <okim@student.42seoul.kr>
2020-11-25 21:24:19 +0900

    ft_atoi modified 201125 21:24

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,33 +6,43 @@
 /*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 23:08:32 by okim              #+#    #+#             */
-/*   Updated: 2020/11/18 23:42:46 by okim             ###   ########.fr       */
+/*   Updated: 2020/11/25 21:04:58 by okim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *s)
+static int	is_space(char c)
 {
-	int	rtn;
-	int	sign;
+	if (c == ' ' || c == '\n' || c == '\t' ||
+		c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	else
+		return (0);
+}
+
+int			ft_atoi(const char *s)
+{
+	int		rtn;
+	int		sign;
+	int		i;
 
 	rtn = 0;
 	sign = 1;
-	if (s[0] == 43)
+	i = 0;
+	while (s[i] && is_space(s[i]) == 1)
+		i++;
+	if (s[i] == '+')
 		s++;
-	else if (s[0] == 45)
+	else if (s[i] == '-')
 	{
 		s++;
 		sign = -1;
 	}
-	while (*s)
+	while (s[i] && s[i] >= 48 && s[i] <= 57)
 	{
-		if (*s >= 48 && *s <= 57)
-			rtn = rtn * 10 + *s - 48;
-		else
-			break ;
-		s++;
+		rtn = rtn * 10 + (s[i] - 48);
+		i++;
 	}
 	return (sign * rtn);
 }
--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,18 +6,20 @@
 /*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 20:19:35 by okim              #+#    #+#             */
-/*   Updated: 2020/11/17 22:26:08 by okim             ###   ########.fr       */
+/*   Updated: 2020/11/25 21:18:48 by okim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_memcmp(const void *s1, const void *s2, size_t n)
+int			ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
 
+	if ((s1 == 0 && s2 == 0) || n == 0)
+		return (0);
 	i = 0;
-	while (i < n)
+	while (n--)
 	{
 		if (*((char *)s1 + i) != *((char *)s2 + i))
 			return (*((char *)s1 + i) - *((char *)s2 + i));

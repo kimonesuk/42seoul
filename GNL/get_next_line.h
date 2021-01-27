c61bbce1fdeefec01c17982c9b4a969c781181eb
okim <okim@student.42seoul.kr>
2021-01-27 09:46:13 +0900

    210127 09:45 make GNL files

---
+++ GNL/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 09:43:29 by okim              #+#    #+#             */
+/*   Updated: 2021/01/27 09:45:44 by okim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	get_next_line(int fd, char **line);
+
---
+++ GNL/main.c
@@ -0,0 +1,16 @@
+int	main(void)
+{
+	char	*line = 0;
+	int		ret;
+	int		fd;
+
+	fd = open("testfile", O_RDONLY);
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("%s\n", line);
+	free(line);
+	return (0);
+}

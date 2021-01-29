c78897153281989a395963b7715d5795a40229a7
okim <okim@student.42seoul.kr>
2021-01-29 10:35:03 +0900

    Test functions 210129 10:35

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 09:41:22 by okim              #+#    #+#             */
-/*   Updated: 2021/01/27 09:42:21 by okim             ###   ########.fr       */
+/*   Updated: 2021/01/29 09:42:01 by okim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 int	get_next_line(int fd, char **line)
 {
-	return (0);
+	static char	buff[BUFFER_SIZE];
+	
+	if (fd == 2)
+		return (-1);
+	else
+	{
+		if (read(fd, buff, BUFFER_SIZE) == -1)
+			return (-1);
+		else if (read(fd, buff, BUFFER_SIZE) == 0)
+			return (0);
+		else
+		{
+
+		}
+	}
 }

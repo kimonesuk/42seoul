3b33390a8e22ba7b3400f6f232103fa78ea83b17
okim <okim@student.42seoul.kr>
2021-02-03 12:26:04 +0900

    GNL 210203 12:26

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 09:41:22 by okim              #+#    #+#             */
-/*   Updated: 2021/02/02 18:24:15 by okim             ###   ########.fr       */
+/*   Updated: 2021/02/03 12:25:49 by okim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,20 @@ char	*ft_strcpy_slice(char *str, size_t start, size_t end)
 
 	size = end - start + 1;
 	idx = 0;
-	if (ft_strlen(str) > start && size > 0)
+	if (start == 0)
+		str = ft_strdup("");
+	if (ft_strlen(str) > start && size > 1)
 	{
 		while (size-- && str[idx + start] != '\0')
-		{	
+		{
 			str[idx] = str[idx + start];
 			idx++;
 		}
-		str[idx] = '\0';
+		while (str[idx])
+		{
+			str[idx] = '\0';
+			idx++;
+		}
 	}
 	return (str);
 }
@@ -45,33 +51,27 @@ size_t	idx_newline(const char *s)
 	return (0);
 }
 
-int	get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-	char		buff[BUFFER_SIZE];
-	static char	*stored[FD_MAX];
-	size_t		readsize;
-	size_t		idx;
+	char			buff[BUFFER_SIZE];
+	static char		*stored[FD_MAX];
+	size_t			readsize;
+	size_t			idx;
 
 	if (fd == 2 || fd < 0 || BUFFER_SIZE <= 0)
 		return (-1);
-	while ((readsize = read(fd, buff, BUFFER_SIZE)) > 0)
+	do
 	{
+		readsize = read(fd, buff, BUFFER_SIZE);
 		buff[readsize] = '\0';
 		stored[fd] = ft_strjoin(stored[fd], buff);
 		if ((idx = idx_newline(stored[fd])) != 0)
 		{
 			*line = (char*)malloc(sizeof(char) * (idx + 2));
-			strlcpy(*line, stored[fd], idx + 1);
+			ft_strlcpy(*line, stored[fd], idx + 1);
 			ft_strcpy_slice(stored[fd], idx + 1, ft_strlen(stored[fd]));
 			return (1);
 		}
-	}
-	if ((idx = idx_newline(stored[fd])) != 0)
-	{
-		*line = (char*)malloc(sizeof(char) * (idx + 2));
-		strlcpy(*line, stored[fd], idx + 1);
-		ft_strcpy_slice(stored[fd], idx + 1, ft_strlen(stored[fd]));
-		return (1);
-	}
+	} while (readsize > 0 || ft_strlen(stored[fd]) != 0)
 	return (0);
 }

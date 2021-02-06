6cca7d65f3878cad290fccee976ad0ddd9991bd4
okim <okim@student.42seoul.kr>
2021-02-07 04:26:58 +0900

    GNL bonus added 210207 04:26

---
+++ GNL/get_next_line_bonus.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 09:41:22 by okim              #+#    #+#             */
+/*   Updated: 2021/02/07 04:26:16 by okim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	chk_slice(char **line, char **stored, size_t idx)
+{
+	char	*tmp;
+
+	if (*stored && idx > (size_t)-1)
+	{
+		*stored[idx] = '\0';
+		*line = ft_strdup(*stored);
+		if (*(*stored + idx + 1))
+			tmp = ft_strdup(*stored + idx + 1);
+		else
+			tmp = 0;
+		free(*stored);
+		*stored = tmp;
+		return (1);
+	}
+	else if (*stored)
+	{
+		*line = *stored;
+		*stored = 0;
+	}
+	else
+		*line = ft_strdup("");
+	return (0);
+}
+
+size_t	idx_newline(const char *s)
+{
+	size_t	idx;
+
+	idx = 0;
+	while (s[idx])
+	{
+		if (s[idx] == '\n')
+			return (idx);
+		idx++;
+	}
+	return (-1);
+}
+
+int		get_next_line_bonus(int fd, char **line)
+{
+	char			buff[BUFFER_SIZE + 1];
+	static char		*stored[FD_MAX];
+	size_t			readsize;
+	size_t			idx;
+
+	if (fd < 0 || line == 0 || BUFFER_SIZE <= 0)
+		return (-1);
+	while ((readsize = read(fd, buff, BUFFER_SIZE)) > (size_t)0)
+	{
+		buff[readsize] = '\0';
+		stored[fd] = ft_strjoin(stored[fd], buff);
+		if ((idx = idx_newline(stored[fd])) > (size_t)-1)
+			return (chk_slice(line, &stored[fd], idx));
+	}
+	if (readsize < (size_t)0)
+		return (-1);
+	return (chk_slice(line, &stored[fd], idx));
+}
---
+++ GNL/get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 09:43:29 by okim              #+#    #+#             */
+/*   Updated: 2021/02/07 04:26:29 by okim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define FD_MAX 256
+
+int		get_next_line_bonus(int fd, char **line);
+char	*ft_strjoin(char *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+size_t	chk_slice(char **line, char **stored, size_t idx);
+size_t	idx_newline(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *str);
+
+#endif

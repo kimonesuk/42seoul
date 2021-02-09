82bb468fe224d425c1351982f3610fb96a6dad0c
okim <okim@student.42seoul.kr>
2021-02-09 18:28:51 +0900

    GNL+bonus tester finished 210209 18:28

---
+++ GNL/GNL_lover/copy_in_here_GNL_files/get_next_line_utils_bonus.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 09:42:49 by okim              #+#    #+#             */
+/*   Updated: 2021/02/09 17:51:53 by okim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strjoin(char *s1, char const *s2)
+{
+	char	*jnstr;
+	int		len1;
+	int		len2;
+
+	if (!s1 && !s2)
+		return (0);
+	else if (!s1)
+		return (ft_strdup(s2));
+	else if (!s2)
+		return (ft_strdup(s1));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (!(jnstr = (char*)malloc(sizeof(char) * (len1 + len2 + 1))))
+		return (0);
+	ft_strlcpy(jnstr, s1, len1 + 1);
+	free(s1);
+	ft_strlcat(jnstr, s2, len1 + len2 + 1);
+	return (jnstr);
+}
+
+int		ft_strlcpy(char *dst, const char *src, int dstsize)
+{
+	int	i;
+
+	if (!dst || !src)
+		return (0);
+	i = 0;
+	while (dstsize > 0)
+	{
+		while (--dstsize > 0 && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	while (src[i])
+		i++;
+	return (i);
+}
+
+int		ft_strlcat(char *dst, const char *src, int dstsize)
+{
+	int	i;
+	int	dst_len;
+	int	src_len;
+
+	i = 0;
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	if (dstsize == 0)
+		return (src_len);
+	while (src[i] != 0 && i + dst_len < dstsize - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	if (dstsize >= dst_len + i)
+		dst[dst_len + i] = 0;
+	if (dst_len < dstsize)
+		return (dst_len + src_len);
+	return (dstsize + src_len);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*rtn;
+	int		len;
+	int		i;
+
+	len = ft_strlen(s1);
+	if (!(rtn = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		rtn[i] = s1[i];
+		i++;
+	}
+	rtn[i] = '\0';
+	return (rtn);
+}
+
+int		ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (*(s++))
+		len++;
+	return (len);
+}
---
+++ GNL/gnl_tester/get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okim <okim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 09:43:29 by okim              #+#    #+#             */
+/*   Updated: 2021/02/09 18:05:40 by okim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define FD_MAX 32
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+int		chk_slice(char **line, char **stored, int idx);
+int		idx_newline(const char *str);
+int		ft_strlcpy(char *dst, const char *src, int dstsize);
+int		ft_strlcat(char *dst, const char *src, int dstsize);
+int		ft_strlen(const char *str);
+
+#endif

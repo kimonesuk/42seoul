1209f41f1563f7f2f7c2d31a7f72779d9bf23640
okim <okim@student.42seoul.kr>
2021-03-17 15:21:43 +0900

    210317 15:21

--- ft_printf/format_parser.c
+++ ft_printf/format_parser.c
@@ -15,15 +15,21 @@
 int	format_parser(char **format, t_format *structs, va_list arg)
 {
 	char	specifiers[13];
+	char	*ptr;
 
+	ft_strlcpy(ptr, *format, ft_strlen(*format) + 1);
 	ft_strlcpy(specifiers, "diucspxX%nfeg", 14);
-	while (ft_strchr(specifiers, **format) == 0)
+	printf("%s, %p, %c\n", *format, *format, **format);
+	/*
+	while (ft_strchr(specifiers, *ptr) == 0)
 	{
 		printf("while is work!");
-		(*format)++;
+		(ptr)++;
 	}
-	printf("\nbefore *format : %p, %c\n", *format, *format);
-	(*format)++;
-	printf("after *format : %p, %c\n", *format, *format);
+	printf("\nbefore ptr : %p, %c\n", ptr, ptr);
+	(ptr)++;
+	printf("after ptr : %p, %c\n", ptr, ptr);
+	*format = ptr;
+	*/
 	return (0);
 }
--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -26,13 +26,14 @@ int	ft_printf(const char *format, ...)
 	str = (char*)format;
 	while ((target = ft_strchr(str, '%')))
 	{
-		printf("before target : %p, %c\n", target, target);
+		printf("before target : %p, %c\n", target, *target);
 		rtn += print_str(str, target - str);
-		structs = (t_format*)malloc(sizeof(t_format));
-		rtn += format_parser(&target, structs, arg);
+		target++;
+		//structs = (t_format*)malloc(sizeof(t_format));
+		//rtn += format_parser(&target, structs, arg);
 		//rtn += print_format(&target, structs, arg);
-		printf("after target : %p, %c\n", target, target);
-		free(structs);
+		//printf("after target : %p, %c\n", target, target);
+		//free(structs);
 	}
 	rtn += print_str(str, ft_strlen(str));
 	va_end(arg);

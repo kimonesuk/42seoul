f0a35f08a3bb4c723cee1f4b1f6eb982d2b08bea
okim <okim@student.42seoul.kr>
2021-04-03 01:52:07 +0000

    run.sh editing

--- ft_server/srcs/run.sh
+++ ft_server/srcs/run.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-service nginx start
+#ssl, redirection, autoindex, php-fpm connection settings
+openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Okim/CN=localhost" -keyout localhost.dev.key -out localhost.dev.crt
+mv localhost.dev.crt etc/ssl/certs/
+mv localhost.dev.key etc/ssl/private/
+chmod 600 etc/ssl/certs/localhost.dev.crt etc/ssl/private/localhost.dev.key
+mv ./tmp/default ./etc/nginx/sites-available/
+
+#mysql, phpmyadmin settings
+tar -xvf ./tmp/phpmyadmin.tar.gz
+mv ./tmp/phpmyadmin ./var/www/html/
+cp ./tmp/config.inc.php ./var/www/html/phpmyadmin/
+service mysql start # mysql start
+mysql < var/www/html/phpmyadmin/sql/create_tables.sql -u root - -skip-password
+
+#wordpress settings
+tar -xvf ./tmp/wordpress.tar.gz
+mv ./tmp/wordpress ./var/www/html/
+cp ./tmp/wp-config.php ./var/www/html/wordpress/
+chown -R www-data:www-data /var/www/html/wordpress
+
+service php7.3-fpm start # start php-fpm
+service nginx start # start nginx
 

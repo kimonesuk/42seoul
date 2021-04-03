650f5b7ddc4163b4c295243b26f91f4f84a65394
okim <okim@student.42seoul.kr>
2021-04-04 00:18:31 +0900

    ft_server finished

--- ft_server/srcs/run.sh
+++ ft_server/srcs/run.sh
@@ -8,26 +8,24 @@ chmod 600 etc/ssl/certs/localhost.dev.crt etc/ssl/private/localhost.dev.key
 mv ./tmp/default ./etc/nginx/sites-available/
 
 #mysql, phpmyadmin settings
-tar -xvf ./tmp/phpmyadmin.tar.gz -C ./tmp/
+tar -xf ./tmp/phpmyadmin.tar.gz -C ./tmp/
 mv ./tmp/phpMyAdmin-5.1.0-all-languages ./tmp/phpmyadmin
 mv ./tmp/phpmyadmin ./var/www/html/
 cp ./tmp/config.inc.php ./var/www/html/phpmyadmin/
-service php7.3-fpm start # start php-fpm
+service php7.3-fpm start # before sql, php-fpm start
 service mysql start # mysql start
 mysql < var/www/html/phpmyadmin/sql/create_tables.sql
 mysqladmin -u root -p password
-mysql
-#create database if not exists wordpress;
-#exit
-service mysql restart
+mysql -e "create database if not exists wordpress"
+service mysql reload # mysql reload
 
 #wordpress settings
-tar -xvf ./tmp/wordpress.tar.gz -C ./tmp/
+tar -xf ./tmp/wordpress.tar.gz -C ./tmp/
 mv ./tmp/wordpress ./var/www/html/
 cp ./tmp/wp-config.php ./var/www/html/wordpress/
 chown -R www-data:www-data /var/www/html/wordpress
 
-service php7.3-fpm restart
+service php7.3-fpm reload # php reload
 service nginx start # start nginx
 
 bash
